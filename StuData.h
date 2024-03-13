2d2b4a25386fecffd96965c49d01c84f20f9dbdf
CaiZeNan <52345728+StrangeMP@users.noreply.github.com>
2024-03-13 14:28:32 +0800

    Move relative functions to Student.c

--- StuData.h
+++ StuData.h
@@ -1,19 +1,4 @@
 #pragma once
 #include "Student.h"
 
-// Global pointers to loaded data
-// Globally used.
-struct {
-    StuNode *pStuHead;
-    StuNode *pStuFoot;
-    Course *pCourseHead;
-    Course *pCourseFoot;
-} LoadResult = {NULL, NULL, NULL, NULL};
 
-// Load data from a binary file.
-// Data to load and build includes:
-//      1) Student, Course and Awards
-//      2) nameIndex and idIndex
-void LoadData(FILE *bin);
-
-void ImportData(FILE *json);
