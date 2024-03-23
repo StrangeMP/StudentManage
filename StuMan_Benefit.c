03c562b575f830e729bf18a907efde469745365a
Chawswes <PatrickMaosignup@outlook.com>
2024-03-23 11:45:58 +0800

    修改了执勤啊提交函数存在的问题

--- StuMan_Benefit.c
+++ StuMan_Benefit.c
@@ -2,23 +2,19 @@
 #include<stdlib.h>
 #include"StuMan_Student.h"
 #include"StuMan_Benefit.h"
+#include"StuMan_Search.h"
 void *Student_AddEssay(int stu_id, Essay *essays)
-{
-    // 先找到对应学生节点
-    Student *student = Get_Student_by_id(stu_id);
-
+{   
+    Student* student =Get_Student_by_id(stu_id);
+    student=(Student*)malloc(sizeof(student));
     if (student != NULL)
     {
-        // 分配内存给加分论文
         student->Benefits.essays= (Essay *)malloc(sizeof(essays));
         if (student->Benefits.essays == NULL)
         {
-            // 内存分配失败
             printf("Memory allocation failed for essay.\n");
             return NULL;
         }
-
-        // 将加分论文添加到学生的加分结构中
         if (student->Benefits.essays == NULL)
         {
             essays->next = NULL;
@@ -27,32 +23,26 @@ void *Student_AddEssay(int stu_id, Essay *essays)
         else
         {
             essays->next = student->Benefits.essays;
-            essays->prev = NULL; // 假设这篇论文将成为新的头部
+            essays->prev = NULL; 
             student->Benefits.essays->prev = essays;
             student->Benefits.essays = essays;
         }
-
-        // 返回成功添加的加分论文指针
         return essays;
     }
 }
 
 void *Student_AddProject(int stu_id, Project *project){
-    // 先找到对应学生节点
     Student *student = Get_Student_by_id(stu_id);
-
+    student=(Student*)malloc(sizeof(student));
     if (student != NULL)
     {
-        // 分配内存给加分论文
         student->Benefits.projects= (Project *)malloc(sizeof(project));
         if (student->Benefits.projects== NULL)
         {
-            // 内存分配失败
             printf("Memory allocation failed for essay.\n");
             return NULL;
         }
 
-        // 将加分论文添加到学生的加分结构中
         if (student->Benefits.projects == NULL)
         {
             project->next = NULL;
@@ -60,29 +50,25 @@ void *Student_AddProject(int stu_id, Project *project){
         }
         else
         {
-            project->next= student->Benefits.essays;
-            project->prev = NULL; // 假设这篇论文将成为新的头部
+            project->next= student->Benefits.projects;
+            project->prev = NULL; 
             student->Benefits.projects->prev = project;
             student->Benefits.projects = project;
         }
 
-        // 返回成功添加的加分论文指针
         return project;
     }
 }
 
 
 void *Student_AddAward(int stu_id, Award *award){
-        // 先找到对应学生节点
     Student *student = Get_Student_by_id(stu_id);
-
+    student=(Student*)malloc(sizeof(student));
     if (student != NULL)
     {
-
         student->Benefits.awards= (Award *)malloc(sizeof(award));
         if (student->Benefits.awards == NULL)
         {
-        
             printf("Memory allocation failed for essay.\n");
             return NULL;
         }
@@ -95,12 +81,11 @@ void *Student_AddAward(int stu_id, Award *award){
         else
         {
             award->next = student->Benefits.awards;
-            award->prev = NULL; 
+            award->prev = NULL;
             student->Benefits.awards->prev = award;
             student->Benefits.awards = award;
         }
 
-        // 返回成功添加的加分论文指针
         return award;
     }
 }
