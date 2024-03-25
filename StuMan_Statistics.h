325199a16062ac6076b6716a2b504d546c32ed42
StrangeMP <52345728+StrangeMP@users.noreply.github.com>
2024-03-25 21:19:25 +0800

    Bug fixes

--- StuMan_Statistics.h
+++ StuMan_Statistics.h
@@ -3,7 +3,7 @@
 void Student_Update(int stu_id);
 
 // 更新某学院某专业某年级学生的排名
-void UpdateRank(int college_num, const char *professionName);
+void UpdateRank(const int college_num, const int grade, const char *professionName);
 
 // 更新课程的优秀率、合格率
 void Course_Update(const char *course_id);
