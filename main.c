4fdd074dcba8dc5d6f9456892f71a4181fd10213
StrangeMP <52345728+StrangeMP@users.noreply.github.com>
2024-04-03 07:26:28 +0800

    Add Teacher_SignUp

--- main.c
+++ main.c
@@ -1,7 +1,9 @@
+#include "StuMan_Account.h"
 #include "StuMan_Import.h"
 #include "StuMan_Server.h"
 int main() {
     ImportData("TestData_Benefits.json");
+    Teacher_Signup("Teachers.json");
     RunServer();
     ReleaseResource();
     return 0;
