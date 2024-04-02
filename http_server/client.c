324d1b570c9314ef3c8e6cf37838116785fbbc86
pudong2979698918 <spf5606@foxmail.com>
2024-04-02 13:05:39 +0800

    删除了无法使用http通信的库

--- http_server/client.c
+++ http_server/client.c
@@ -1,5 +1,4 @@
 #include"tcpSocket.h"
-#include"fileTrans.h"
 
 #define PORT 1234
 
@@ -10,7 +9,6 @@ int main()
     SOCKET fd=connect_client_to("127.0.0.1",PORT);//use AF_INET——ipv4
     
     //行为
-    recvfile(fd,"pic_received.jpg");
     printf("tap any key to disconnect\n");
     getchar();
     send(fd,"disconnect",10,0);
