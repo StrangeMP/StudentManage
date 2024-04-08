42c1acc9016a69c2d26ad45542c9d19d5799837d
StrangeMP <52345728+StrangeMP@users.noreply.github.com>
2024-04-08 22:35:49 +0800

    reload from TestData.json

--- reload.c
+++ reload.c
@@ -1,8 +1,11 @@
 #include "StuMan_Binary.h"
 #include "StuMan_Import.h"
 
-int main() {
-    ImportData("TestData_Benefits.json");
+int main(int argc, char **argv) {
+    if (argc != 2)
+        ImportData("TestData.json");
+    else
+        ImportData(argv[1]);
     SaveData();
     ReleaseResource();
     return 0;
