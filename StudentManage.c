1bae4b40254a4cc5ba0d76d6c0ca511d6cfc5fb5
StrangeMP <52345728+StrangeMP@users.noreply.github.com>
2024-04-08 18:40:01 +0800

    Add import teacher accounts

--- StudentManage.c
+++ StudentManage.c
@@ -1,8 +1,13 @@
-﻿#include "StuMan_Binary.h"
+﻿#include "StuMan_Account.h"
+#include "StuMan_Binary.h"
 #include "StuMan_Server.h"
 
-int main() {
-    LoadData("__Stu_Info.bin", "__Cor_Info.bin");
+int main(int argc, char **argv) {
+    if (argc != 3)
+        LoadData("__Stu_Info.bin", "__Cor_Info.bin");
+    else
+        LoadData(argv[1], argv[2]);
+    Teacher_Signup("Teachers.json");
     RunServer();
     SaveData();
     ReleaseResource();
